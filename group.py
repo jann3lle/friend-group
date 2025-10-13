3163e8fd98fe3df463f82d0ee674041b04d25f4c
Jannelle <zcemjof@ucl.ac.uk>
2025-10-13 10:53:43 +0100

    Improved formatting in New branch (stretch task)

--- group.py
+++ group.py
@@ -3,11 +3,19 @@
 # Your code to go here...
 
 my_group = {
-    "Jill": {"age": 26, "job": "Biologist", "connection": {"friend": "Zalika", "partner": "John"}},
+    "Jill": {
+        "age": 26,
+        "job": "Biologist",
+        "connection": {"friend": "Zalika", "partner": "John"},
+    },
     "Zalika": {"age": 28, "job": "Artist", "connection": {"friend": "Jill"}},
     "John": {"age": 27, "job": "Writer", "connection": {"partner": "Jill"}},
-    "Nash": {"age": 34, "job": "Chef", "connection": {"cousin": "John", "landlord": "Zalika"}},
-    "Lila": {"age":"" , "job": "", "connection": ""},
-    "Nelly": {"age":22, "job": "Student", "connection":""},
-    "Zara": {"age": 22, "job": "Student", "connection": {"friend": "Lila"}}
-    }
+    "Nash": {
+        "age": 34,
+        "job": "Chef",
+        "connection": {"cousin": "John", "landlord": "Zalika"},
+    },
+    "Lila": {"age": "", "job": "", "connection": ""},
+    "Nelly": {"age": 22, "job": "Student", "connection": ""},
+    "Zara": {"age": 22, "job": "Student", "connection": {"friend": "Lila"}},
+}
